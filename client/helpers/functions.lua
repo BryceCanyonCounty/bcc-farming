66284712f8e7c1c9944f780534c639ff273770cb
Apollyon <37031311+JusCampin@users.noreply.github.com>
2024-08-27 00:21:36 -0700

    Update functions.lua
    - removed unused drawtext function
    - housekeeping

--- client/helpers/functions.lua
+++ client/helpers/functions.lua
@@ -4,34 +4,21 @@ BccUtils = exports['bcc-utils'].initiate()
 ---@param hash string
 ---@param time integer --Milliseconds
 function ScenarioInPlace(hash, time)
-    local pl = PlayerPedId()
-    FreezeEntityPosition(pl, true)
-    TaskStartScenarioInPlace(pl, joaat(hash), time, true, false, false, false)
+    local playerPed = PlayerPedId()
+    FreezeEntityPosition(playerPed, true)
+    TaskStartScenarioInPlace(playerPed, joaat(hash), time, true, false, false, false)
     Wait(time)
-    ClearPedTasksImmediately(pl)
-    FreezeEntityPosition(pl, false)
-end
-
----@param x number
----@param y number
----@param z number
----@param text string
-function DrawText3D(x, y, z, text)
-    local onScreen, screenX, screenY = GetScreenCoordFromWorldCoord(x, y, z)
-
-    SetTextScale(0.35, 0.35)
-    SetTextFontForCurrentCommand(1)
-    SetTextColor(255, 255, 255, 223)
-    SetTextCentre(1)
-    DisplayText(CreateVarString(10, "LITERAL_STRING", text), screenX, screenY)
+    ClearPedTasksImmediately(playerPed)
+    FreezeEntityPosition(playerPed, false)
 end
 
 function PlayAnim(animDict, animName, time, raking, loopUntilTimeOver) --function to play an animation
     local animTime = time
     RequestAnimDict(animDict)
     while not HasAnimDictLoaded(animDict) do
-        Wait(100)
+        Wait(10)
     end
+
     local flag = 16
     -- if time is -1 then play the animation in an infinite loop which is not possible with flag 16 but with 1
     -- if time is -1 the caller has to deal with ending the animation by themselve
@@ -39,17 +26,19 @@ function PlayAnim(animDict, animName, time, raking, loopUntilTimeOver) --functio
         flag = 1
         animTime = -1
     end
-    TaskPlayAnim(PlayerPedId(), animDict, animName, 1.0, 1.0, animTime, flag, 0, true, 0, false, 0, false)
+
+    local playerPed = PlayerPedId()
+    TaskPlayAnim(playerPed, animDict, animName, 1.0, 1.0, animTime, flag, 0, true, 0, false, 0, false)
     if raking then
-        local playerCoords = GetEntityCoords(PlayerPedId())
-        local rakeObj = CreateObject("p_rake02x", playerCoords.x, playerCoords.y, playerCoords.z, true, true, false)
-        AttachEntityToEntity(rakeObj, PlayerPedId(), GetEntityBoneIndexByName(PlayerPedId(), "PH_R_Hand"), 0.0, 0.0, 0.19,
+        local playerCoords = GetEntityCoords(playerPed)
+        local rakeObj = CreateObject('p_rake02x', playerCoords.x, playerCoords.y, playerCoords.z, true, true, false)
+        AttachEntityToEntity(rakeObj, playerPed, GetEntityBoneIndexByName(playerPed, 'PH_R_Hand'), 0.0, 0.0, 0.19,
             0.0, 0.0, 0.0, false, false, true, false, 0, true, false, false)
         Wait(time)
         DeleteObject(rakeObj)
-        ClearPedTasksImmediately(PlayerPedId())
+        ClearPedTasksImmediately(playerPed)
     else
         Wait(time)
-        ClearPedTasksImmediately(PlayerPedId())
+        ClearPedTasksImmediately(playerPed)
     end
 end
