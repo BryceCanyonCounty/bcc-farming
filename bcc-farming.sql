f575d8dc2de141689a0b9c5298b7d7b8446e9b52
Apollyon <37031311+JusCampin@users.noreply.github.com>
2024-10-08 20:31:12 -0700

    Update bcc-farming.sql
    - combined inserts into one
    - housekeeping

--- bcc-farming.sql
+++ bcc-farming.sql
@@ -1,25 +1,27 @@
 CREATE TABLE IF NOT EXISTS `bcc_farming` (
-	`plant_id` INT(40) NOT NULL AUTO_INCREMENT,
-    `plant_coords` LONGTEXT NOT NULL,
-    `plant_type` VARCHAR(40) NOT NULL,
-    `plant_watered` char(6) NOT NULL DEFAULT 'false',
-    `time_left` varchar(100) NOT NULL,
-    `plant_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `plant_owner` INT(40) NOT NULL,
-	PRIMARY KEY (`plant_id`)
+  `plant_id` INT(40) NOT NULL AUTO_INCREMENT,
+  `plant_coords` LONGTEXT NOT NULL,
+  `plant_type` VARCHAR(40) NOT NULL,
+  `plant_watered` CHAR(6) NOT NULL DEFAULT 'false',
+  `time_left` VARCHAR(100) NOT NULL,
+  `plant_time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `plant_owner` INT(40) NOT NULL,
+  PRIMARY KEY (`plant_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('wateringcan', 'Water Jug', 10, 1, 'item_standard', 1)
-    ON DUPLICATE KEY UPDATE `item`='wateringcan', `label`='Water Jug', `limit`=10, `can_remove`=1, `type`='item_standard', `usable`=1;
+INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`, `desc`)
+VALUES
+  ('wateringcan', 'Water Jug', 10, 1, 'item_standard', 1, NULL),
+  ('wateringcan_empty', 'Empty Watering Jug', 10, 1, 'item_standard', 1, NULL),
+  ('fertilizer1', 'Fertilizer Grade C', 10, 1, 'item_standard', 1, 'Low grade fertilizer.'),
+  ('fertilizer2', 'Fertilizer Grade B', 10, 1, 'item_standard', 1, 'Mid grade fertilizer.'),
+  ('fertilizer3', 'Fertilizer Grade A', 10, 1, 'item_standard', 1, 'High grade fertilizer.')
+ON DUPLICATE KEY UPDATE
+  `item` = VALUES(`item`),
+  `label` = VALUES(`label`),
+  `limit` = VALUES(`limit`),
+  `can_remove` = VALUES(`can_remove`),
+  `type` = VALUES(`type`),
+  `usable` = VALUES(`usable`),
+  `desc` = VALUES(`desc`);
 
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`) VALUES ('wateringcan_empty', 'Empty Watering Jug', 10, 1, 'item_standard', 1)
-    ON DUPLICATE KEY UPDATE `item`='wateringcan_empty', `label`='Empty Watering Jug', `limit`=10, `can_remove`=1, `type`='item_standard', `usable`=1;
-
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`, `desc`) VALUES ('fertilizer1', 'Fertilizer Grade C', 10, 1, 'item_standard', 1, 'Low grade fertilizer.')
-    ON DUPLICATE KEY UPDATE `item`='fertilizer1', `label`='Fertilizer Grade C', `limit`=10, `can_remove`=1, `type`='item_standard', `usable`=1, `desc`='Low grade fertilizer.';
-
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`, `desc`) VALUES ('fertilizer2', 'Fertilizer Grade B', 10, 1, 'item_standard', 1, 'Mid grade fertilizer.')
-    ON DUPLICATE KEY UPDATE `item`='fertilizer2', `label`='Fertilizer Grade B', `limit`=10, `can_remove`=1, `type`='item_standard', `usable`=1, `desc`='Mid grade fertilizer.';
-
-INSERT INTO `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`, `desc`) VALUES ('fertilizer3', 'Fertilizer Grade A', 10, 1, 'item_standard', 1, 'High grade fertilizer.')
-    ON DUPLICATE KEY UPDATE `item`='fertilizer3', `label`='Fertilizer Grade A', `limit`=10, `can_remove`=1, `type`='item_standard', `usable`=1, `desc`='High grade fertilizer.';
